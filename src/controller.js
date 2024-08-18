efdaec9b4c8b28472f3e9174d5f0094fe14845c5
mili vidal <milagrosvdal@gmail.com>
2024-08-18 10:00:47 -0300

    Agrega función add para añadir libros

--- src/controller.js
+++ src/controller.js
@@ -5,6 +5,16 @@ class LibroController {
     const [result] = await pool.query("SELECT * FROM libros");
     res.json(result);
   }
+
+  async add(req, res) {
+    const libro = req.body;
+    const [result] = await pool.query(
+      `INSERT INTO Libros(nombre, autor, categoria, fecha, isbn) VALUES (?, ?, ?, ?, ?)`,
+      [libro.nombre, libro.autor, libro.categoria, libro.fecha, libro.isbn]
+    );
+
+    res.json({ "Id insertado": result.insertId });
+  }
 }
 
 export const libro = new LibroController();
